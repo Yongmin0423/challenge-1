49f7d5b6f23887ee01551f3ba402a05730784e19
Yongmin0423 <yym21345@gmail.com>
2025-09-29 19:32:08 +0900

    [유용민] Ad카드 내 상담카드 반응형 수정

--- components/adCard/AdCard.tsx
+++ components/adCard/AdCard.tsx
@@ -33,21 +33,21 @@ export default function AdCard({
           <h4>{price}</h4>
           <button>주문하러 가기</button>
         </div>
-      </div>
-      <div className={cx("bottom")}>
-        <div className={cx("bottom-content")}>
-          <h3>
-            <Telephone /> 디자인 작업이 필요하신가요?
-          </h3>
-          <div>
-            <p>제트디자인 디자이너가 맞춤형 디자인을 제작해 드려요.</p>
-            <p>상담 후 상품명을 제작해 보세요.</p>
+        <div className={cx("bottom")}>
+          <div className={cx("bottom-content")}>
+            <h3>
+              <Telephone /> 디자인 작업이 필요하신가요?
+            </h3>
+            <div>
+              <p>제트디자인 디자이너가 맞춤형 디자인을 제작해 드려요.</p>
+              <p>상담 후 상품명을 제작해 보세요.</p>
+            </div>
           </div>
+          <button className={cx("bottom-button")}>
+            <Chat />
+            <p>디자인 상담</p>
+          </button>
         </div>
-        <button className={cx("bottom-button")}>
-          <Chat />
-          <p>디자인 상담</p>
-        </button>
       </div>
     </div>
   );
