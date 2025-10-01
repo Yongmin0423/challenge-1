0622449b04e28e260a26566ea9b41cfeea78825c
Yongmin0423 <yym21345@gmail.com>
2025-10-01 20:31:22 +0900

    [유용민] 모바일 반응형 css

--- components/header/HeaderM.tsx
+++ components/header/HeaderM.tsx
@@ -3,34 +3,53 @@
 import Image from "next/image";
 import cn from "classnames/bind";
 import styles from "./HeaderM.module.scss";
-import LogoM from "@/assets/images/LogoM.png";
 import Hamberger from "@/assets/icons/Hamberger";
 import { useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { getProductById } from "@/data/products";
+import ArrowLeft from "@/assets/icons/ArrowLeft";
 
 const cx = cn.bind(styles);
 
 export default function HeaderM() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const pathname = usePathname();
+
+  const router = useRouter();
+
+  const productId = pathname?.slice(1);
+  const product = productId ? getProductById(productId) : null;
+  const isDetailPage = !!product;
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const handleBack = () => {
+    router.back();
+  };
+
   return (
     <>
-      <div className={cx("header-mobile")}>
-        <div className={cx("logo")}>
-          <Image src={LogoM} alt="logo" />
-        </div>
+      <div className={cx("header-mobile", { detail: isDetailPage })}>
+        {isDetailPage ? (
+          <div>
+            <button className={cx("back-btn")} onClick={handleBack}>
+              <ArrowLeft />
+            </button>
+          </div>
+        ) : (
+          <div className={cx("logo")}>ZDESIGN</div>
+        )}
+        <div className={cx("title")}>{product?.title}</div>
+
         <button className={cx("hamburger-btn")} onClick={toggleMenu}>
           <Hamberger />
         </button>
       </div>
 
       {/* Overlay Background */}
-      {isMenuOpen && (
-        <div className={cx("overlay")} onClick={toggleMenu}></div>
-      )}
+      {isMenuOpen && <div className={cx("overlay")} onClick={toggleMenu}></div>}
 
       {/* Side Menu */}
       <div className={cx("side-menu", { open: isMenuOpen })}>
